dc4e42e07e8d7d2bc62bccf6727b2ed72a89587e
Lussankya <leducthanh45@gmail.com>
2022-08-24 22:24:58 +0700

    fuck off

---
+++ KarlanTravelClient/Controllers/TourController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KarlanTravelClient.Models;
+using System.Data.Entity;
+
+namespace KarlanTravelClient.Controllers
+{
+    public class TourController : Controller
+    {
+        // GET: Tour
+        ContextModel db = new ContextModel();
+        public ActionResult Index()
+        {
+            var tour = db.Tours.Include(t => t.Category).Include(t => t.Category1);
+            return View(tour.ToList());
+        }
+    }
+}
