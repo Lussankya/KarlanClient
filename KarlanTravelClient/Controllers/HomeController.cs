dd92bfe4d6789afb207849fa1fccc4b8f27eb64d
Lussankya <leducthanh45@gmail.com>
2022-08-24 23:36:33 +0700

    fuck off3

--- KarlanTravelClient/Controllers/HomeController.cs
+++ KarlanTravelClient/Controllers/HomeController.cs
@@ -15,16 +15,35 @@ namespace KarlanTravelClient.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+
 
             return View();
         }
+        public ActionResult Tour()
+        {
+            return View();
+        }
+
+        public ActionResult Customer()
+        {
+
+
+            return View();
+        }
+
+        public ActionResult Transaction()
+        {
+
+
+            return View();
+        }
+
     }
 }
